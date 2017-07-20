8fe78c975833b1a2f93481f1d2e51dbf3399b30a
fsotomayor <francisco.sotomayor@hotmail.com>
2017-07-19 20:22:26 -0400

    tercer commit, prueba2_fsotomayor

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+	has_many :items
+	validates :name, presence: true
+	validates :email, uniqueness: true
+	before_save :upper_name
+
+	def upper_name
+		self.name[0] = self.name[0].upcase!
+	end
+end
