ba4d13c3f5c8f346486d67719a81dee0a972579b
fsotomayor <francisco.sotomayor@hotmail.com>
2017-07-19 20:36:18 -0400

    quinto commit, prueba2_fsotomayor

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user, optional: true
+  
+  scope :sc_size,  (-> (x) { where("size = ?", x) } )
+  scope :sc_no_asignado, (-> { where("user_id is null") } )
 end
